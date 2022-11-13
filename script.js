eb65c5b431e83425a430c6a6b9cf52c2fb863019
aditi <aditi291soni@gmail.com>
2022-11-13 22:42:07 +0530

    Q11

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 var a=Number(prompt("enter mark 1"));
 var b=Number(prompt("enter mark 2"));
-var c = Number(prompt("enter mark 3"));
-if(a==b && b==c){
-console.log("all are eqaul")
-}
-else if(a==b || b==c){
-console.log("any two is equal")
-}
+// var c = Number(prompt("enter mark 3"));
+var d= Math.max(a,b);
+console.log(d);
