3728dcfafadfeccbae49510d072acb1b1fe5366d
aditi <aditi291soni@gmail.com>
2022-11-13 22:57:54 +0530

    Q15

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-var a=prompt("enter a no")
-if(a%2==0){
-    console.log("even no");
+var a=Number(prompt("enter a age"))
+var b=prompt("enter name")
+var c=18-a;
+// console.log(c)
+if(a>18){
+    console.log(b," you can vote")
 }
 else{
-    console.log("odd no")
+    console.log(b," you cannot vote now. You are able to vote after ", c," years" );
 }
