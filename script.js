8e658492c3723fde6d37da3cbad1dee953148ed1
aditi <aditi291soni@gmail.com>
2022-11-14 19:37:18 +0530

    Q19

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-var a=Number(prompt("enter a age"))
-var b=prompt("enter name")
-var c=18-a;
-// console.log(c)
-if(a>18){
-    console.log(b," you can vote")
+var a=Number(prompt("Enter Day no"))
+// var b=Number(prompt("enter a rate"))
+// var c = Number(prompt("enter a time"));
+if (a == 1) {
+  console.log("monday");
+} else if (a == 2) {
+  console.log("tuesday");
+} else if (a == 3) {
+  console.log("wednesday");
+} else if (a == 4) {
+  console.log("thursday");
+} else if (a == 5) {
+  console.log("friday");
+} else if (a == 6) {
+  console.log("saturday");
+} else if (a == 7) {
+  console.log("sunday");
 }
 else{
-    console.log(b," you cannot vote now. You are able to vote after ", c," years" );
+    console.log("enter wrong day");
 }
